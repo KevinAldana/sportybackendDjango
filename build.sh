3612f40908a6b14433eaa2c90128d706baa4f641
Kevin Aldana <64706806+KevinAldana@users.noreply.github.com>
2023-11-14 20:02:56 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
-source venv/bin/activate
 set -o errexit
-
+source venv/bin/activate
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade poetry
 pip install -r requirements.txt
 python -m pip install --upgrade pip
 python manage.py collectstatic --no-input
