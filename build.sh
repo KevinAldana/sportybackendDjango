2b83cccad8e3a43877a67edd249d61bbb5651fd3
Kevin Aldana <64706806+KevinAldana@users.noreply.github.com>
2023-11-14 20:00:19 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
 source venv/bin/activate
-pip install -r requirements.txt
+set -o errexit
 
+pip install -r requirements.txt
+python -m pip install --upgrade pip
 python manage.py collectstatic --no-input
 python manage.py migrate
